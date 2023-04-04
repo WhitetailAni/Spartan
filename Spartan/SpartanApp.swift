36ba6982d8c88d2031e19c9eaadd695e1ac5d575
WhitetailAni <davidmichael2006@gmail.com>
2023-04-03 22:42:29 -0500

    yeet

---
+++ Spartan/SpartanApp.swift
@@ -0,0 +1,24 @@
+//
+//  SpartanApp.swift
+//  Spartan
+//
+//  Created by RealKGB on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let contentView = ContentView()
+        let hostingController = UIHostingController(rootView: contentView)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = hostingController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
