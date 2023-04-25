0bd5d5b0a9a02835eac2be958bce1c6bcb808b05
WhitetailAni <joemama@thanos.lol>
2023-04-24 23:21:50 -0500

    42

--- Spartan/EThree.swift
+++ Spartan/EThree.swift
@@ -13,33 +13,71 @@ struct E3: View {
     @Binding var files: [String]
     @Binding var multiSelectFiles: [String]
     @Binding var fileWasSelected: [Bool]
+    @Binding var showSubView: [Bool]
     
     var body: some View {
     
-        Text("**Welcome to Milliways**")
+        let buttonWidth: CGFloat = 500
+        let buttonHeight: CGFloat = 30
+        let paddingInt: CGFloat = -7
+        let opacityInt: CGFloat = 1.0
+    
+        Text("Welcome to Milliways")
+            .font(.system(size: 69))
+            .bold()
+            .padding(paddingInt)
         
         Button(action: {
             print(directory)
         }) {
             Text("Print 'directory'")
+                .frame(width: buttonWidth, height: buttonHeight)
         }
+        .padding(paddingInt)
+        .opacity(opacityInt)
         
         Button(action: {
-            print(files)
+            print("\(files), \(files.count)")
         }) {
             Text("Print 'files'")
+                .frame(width: buttonWidth, height: buttonHeight)
         }
+        .padding(paddingInt)
+        .opacity(opacityInt)
         
         Button(action: {
-            print(multiSelectFiles)
+            print("\(multiSelectFiles) \(multiSelectFiles.count)")
         }) {
             Text("Print 'multiSelectFiles'")
+                .frame(width: buttonWidth, height: buttonHeight)
         }
+        .padding(paddingInt)
+        .opacity(opacityInt)
         
         Button(action: {
-            print(fileWasSelected)
+            print("\(fileWasSelected) \(fileWasSelected.count)")
         }) {
             Text("Print 'fileWasSelected'")
+                .frame(width: buttonWidth, height: buttonHeight)
         }
+        .padding(paddingInt)
+        .opacity(opacityInt)
+        
+        Button(action: {
+            print("\(showSubView) \(showSubView.count)")
+        }) {
+            Text("Print 'showSubView'")
+                .frame(width: buttonWidth, height: buttonHeight)
+        }
+        .padding(paddingInt)
+        .opacity(opacityInt)
+        
+        Button(action: {
+            print("What is the answer to Life, the Universe, and Everything?")
+        }) {
+            Text("42")
+                .frame(width: buttonWidth, height: buttonHeight)
+        }
+        .padding(10)
     }
 }
