bbeb530560ae79b12917e02b37ddf6ffd8d8dd26
WhitetailAni <joemama@thanos.lol>
2023-04-27 21:11:44 -0500

    fix Open In bug, add preliminary support for applist, improve some methods

---
+++ Spartan/ButtonActions/MountPointsView.swift
@@ -0,0 +1,18 @@
+//
+//  MountPointsView.swift
+//  Spartan
+//
+//  Created by RealKGB on 4/27/23.
+//
+
+import SwiftUI
+
+
+struct MountPointsView: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        Text("hi")
+    }
+}
+
